c6ab260f5d5a4f2b00f7176ac9618866f3fe9160
Oleksandr <bragardlin@gmail.com>
2023-07-18 00:47:34 +0300

    added errors

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,12 +1,16 @@
 import { register, login, logout, refresh } from './operations';
-
-const { createSlice } = require('@reduxjs/toolkit');
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 const initialState = {
   user: {},
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+};
+
+const handleReject = (state, action) => {
+  toast.error(action.payload);
 };
 
 const authSlice = createSlice({
@@ -18,14 +22,19 @@ const authSlice = createSlice({
       state.token = payload.token;
       state.isLoggedIn = true;
     },
+    [login.rejected]: handleReject,
+
     [login.fulfilled](state, { payload }) {
       state.user = payload.user;
       state.token = payload.token;
       state.isLoggedIn = true;
     },
+    [login.rejected]: handleReject,
+
     [logout.fulfilled](state) {
       state.isLoggedIn = false;
     },
+    [login.rejected]: handleReject,
 
     [refresh.pending](state) {
       state.isRefreshing = true;
